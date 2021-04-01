6c46b2860c7f31bee868b4d65a265bd2f3e063cb
Makz mann <mr.greenmousth@gmail.com>
2021-04-01 14:42:39 -0300

    + except KeyboardInterrupt

--- crono.py
+++ crono.py
@@ -1,3 +1,4 @@
+import keyboard
 from time import sleep
 from os import system
 
@@ -6,9 +7,14 @@ while 1:
     
     for m in range(59):
         for s in range(59):
-            system('clear')
-            h = '0'+str(h) if len(str(h)) == 1 else h
-            m = '0'+str(m) if len(str(m)) == 1 else m
-            s = '0'+str(s) if len(str(s)) == 1 else s
-            print(f'{h}:{m}:{s}')
-            sleep(1)
+            try:
+                system('clear')
+                h = '0'+str(h) if len(str(h)) == 1 else h
+                m = '0'+str(m) if len(str(m)) == 1 else m
+                s = '0'+str(s) if len(str(s)) == 1 else s
+                print(f'\n   {h}:{m}:{s}')
+                sleep(1)
+            except KeyboardInterrupt:
+                system('clear')
+                print(f'\n   {h}:{m}:{s}\n')
+                exit()
