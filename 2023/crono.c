b2096f39602257d1ee78cad7697046f209dc3bab
Makz Mann <mr.greenmousth@gmail.com>
2023-09-03 01:07:42 -0300

    add pause functionality

--- 2023/crono.c
+++ 2023/crono.c
@@ -15,6 +15,7 @@ void input(SDL_Event *ev);
 void update(SDL_Renderer *renderer, TTF_Font *font, int *minutes, int *second);
 
 int running = 1;
+int time_paused = 0;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -84,12 +85,69 @@ void input(SDL_Event *ev) {
                 running = 0;
                 break;
             case SDL_KEYDOWN:
-                if (ev->key.keysym.sym == SDLK_q) {
-                    running = 0;
-                    break;
+                switch (ev->key.keysym.sym) {
+                    case SDLK_q: {
+                        running = 0;
+                    } break;
+                    case SDLK_SPACE: {
+                        time_paused = !time_paused;
+                    } break;
                 }
                 break;
         }
+    }
+}
+
+SDL_Color bg = { 0x18, 0x18, 0x18, SDL_ALPHA_OPAQUE };
+SDL_Color fg = {0xcc, 0xcc, 0xcc, SDL_ALPHA_OPAQUE };
+
+void render_background(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
+    SDL_RenderClear(renderer);
+}
+
+void render_time(char *time, TTF_Font *font, SDL_Renderer *renderer) {
+    SDL_Surface *sur = TTF_RenderText_Solid(font, time, fg);
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, sur);
+
+    const int scale = 8;
+    const int x_center = WIN_WIDTH / 2 - sur->w * scale / 2;
+    const int y_center = WIN_HEIGHT / 2 - sur->h * scale / 2;
+
+    SDL_Rect dst = { x_center, y_center, sur->w * scale, sur->h * scale };
+    SDL_RenderCopy(renderer, tex, NULL, &dst);
+
+    SDL_FreeSurface(sur);
+    SDL_DestroyTexture(tex);
+}
+
+void update_time(int *minutes, int *seconds, int *second_cooldown, const int elapsed) {
+    if (*minutes != 0 || *seconds != 0) {
+        if (!time_paused) {
+            *second_cooldown -= elapsed;
+            if (*second_cooldown <= 0) {
+                if (*seconds == 0) (*minutes)--;
+                *seconds = (*seconds + 60 - 1) % 60;
+
+                *second_cooldown = SECOND_COOLDOWN;
+            }
+        }
+    }
+}
+void render_pause_status(TTF_Font *font,SDL_Renderer *renderer, int x, int y) {
+    if (time_paused) {
+        const int scale = 2;
+
+        SDL_Surface *sur = TTF_RenderText_Solid(font, "paused", fg);
+        SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, sur);
+
+        y -= sur->h * scale;
+
+        SDL_Rect dst = { x, y, sur->w * scale, sur->h * scale };
+        SDL_RenderCopy(renderer, tex, NULL, &dst);
+
+        SDL_FreeSurface(sur);
+        SDL_DestroyTexture(tex);
     }
 }
 
@@ -106,37 +164,15 @@ void update(SDL_Renderer *renderer, TTF_Font *font, int *minutes, int *seconds) 
 
     last_render = SDL_GetTicks();
 
-    SDL_Color bg = { 0x18, 0x18, 0x18, SDL_ALPHA_OPAQUE };
-    SDL_Color fg = {0xcc, 0xcc, 0xcc, SDL_ALPHA_OPAQUE };
-
-    SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
-    SDL_RenderClear(renderer);
-
     char time[6];
     sprintf(time, "%02d:%02d", *minutes, *seconds);
 
-    SDL_Surface *sur = TTF_RenderText_Solid(font, time, fg);
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, sur);
-
-    const int scale = 8;
-    const int x_center = WIN_WIDTH / 2 - sur->w * scale / 2;
-    const int y_center = WIN_HEIGHT / 2 - sur->h * scale / 2;
-
-    SDL_Rect dst = { x_center, y_center, sur->w * scale, sur->h * scale };
-    SDL_RenderCopy(renderer, tex, NULL, &dst);
-
-    if (*minutes != 0 || *seconds != 0) {
-        second_cooldown -= elapsed;
-        if (second_cooldown <= 0) {
-            if (*seconds == 0) (*minutes)--;
-            *seconds = (*seconds + 60 - 1) % 60;
-
-            second_cooldown = SECOND_COOLDOWN;
-        }
-    }
-
-    SDL_FreeSurface(sur);
-    SDL_DestroyTexture(tex);
+    render_background(renderer);
+    render_time(time, font, renderer);
+    int rbc_x = 10;
+    int rbc_y = WIN_HEIGHT - 10;
+    render_pause_status(font, renderer, rbc_x, rbc_y);
+    update_time(minutes, seconds, &second_cooldown, elapsed);
 
     SDL_RenderPresent(renderer);
 }
